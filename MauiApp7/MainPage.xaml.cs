ad719c0e94b2b9ef1eecf64b1801d6d9816c153e
ilyaDavidov <ilya.davidov63@mail.ru>
2022-10-24 19:07:27 +0200

    q

---
+++ MauiApp7/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp7;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+
+	}
+}
+
